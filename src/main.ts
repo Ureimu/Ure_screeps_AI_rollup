64103b8e9276f5f344a04cb85dd37d343c1cb2e6
Ureimu <57444811+Ureimu@users.noreply.github.com>
2020-11-08 13:12:57 +0800

    新增profiler以使得cpu消耗可视化。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { globalFunctionRegister } from "mount/mountGlobalFunction";
 import { mountPrototypeExtension } from "mount/mountPrototypeExtension";
 import { ErrorMapper } from "utils/ErrorMapper";
+import profiler from "utils/profiler";
 //import actionCounter from "./utils/actionCounter";
 import { manageTask } from "./task";
 import { initNewRoomSetting } from "./updateMemory";
-import { run } from './work/creep/index';
+import { run } from "./work/creep/index";
 //import './utils/bypass';
 //import { mountCreepEnergyMonitor } from "utils/energyMonitor";
 import { autoConstruction } from "construction";
@@ -15,57 +16,60 @@ import { globalConstantRegister } from "mount/mountGlobalConstant";
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 //export const loop = ErrorMapper.wrapLoop(() => {
+profiler.enable();
 export const loop = () => {
-    try{
-        if(!Memory.errors){
-            Memory.errors={
-                errorCount:[],
-                errorList:[],
-                errorIntervals:[]
+    profiler.wrap(function () {
+        try {
+            if (!Memory.errors) {
+                Memory.errors = {
+                    errorCount: [],
+                    errorList: [],
+                    errorIntervals: []
+                };
             }
-        }
-        mountPrototypeExtension();
-        //mountCreepEnergyMonitor();
-        globalConstantRegister()
-        globalFunctionRegister();
-        //actionCounter.init();
-        if(Game.cpu.bucket > 9000) {
-            if(!!Game.cpu.generatePixel){
-                Game.cpu.generatePixel();
-            }
-        }
-
-        _.forEach(Game.rooms, room => {
-            let eventLog = room.getEventLog();
-            let attackEvents = _.filter(eventLog, {event: EVENT_ATTACK});
-            attackEvents.forEach(event => {
-                if(event.event==EVENT_ATTACK||EVENT_ATTACK_CONTROLLER){
-                    if(!!room && !!room.controller && room.controller.my){
-                        room.controller.activateSafeMode();
-                    }
+            mountPrototypeExtension();
+            //mountCreepEnergyMonitor();
+            globalConstantRegister();
+            globalFunctionRegister();
+            //actionCounter.init();
+            if (Game.cpu.bucket > 9000) {
+                if (!!Game.cpu.generatePixel) {
+                    Game.cpu.generatePixel();
                 }
-            });
-        });
-
-        initNewRoomSetting();
-        manageTask();
-        manageCreep();
-        autoConstruction();
-        roomVisualize();
-        runStructure();
-
-        for (let spawnName in Game.spawns) {
-            if (!Game.spawns[spawnName].spawning) {
-                Game.spawns[spawnName].spawnTask();
             }
-        }
 
-        for(let creepName in Game.creeps) {
-            run(Game.creeps[creepName]);
-        }
+            _.forEach(Game.rooms, room => {
+                let eventLog = room.getEventLog();
+                let attackEvents = _.filter(eventLog, { event: EVENT_ATTACK });
+                attackEvents.forEach(event => {
+                    if (event.event == EVENT_ATTACK || EVENT_ATTACK_CONTROLLER) {
+                        if (!!room && !!room.controller && room.controller.my) {
+                            room.controller.activateSafeMode();
+                        }
+                    }
+                });
+            });
 
-        //actionCounter.save(1500);
-    } catch(err) {
-        errorStackVisualize(err.stack);
-    }
-}//);
+            initNewRoomSetting();
+            manageTask();
+            manageCreep();
+            autoConstruction();
+            roomVisualize();
+            runStructure();
+
+            for (let spawnName in Game.spawns) {
+                if (!Game.spawns[spawnName].spawning) {
+                    Game.spawns[spawnName].spawnTask();
+                }
+            }
+
+            for (let creepName in Game.creeps) {
+                run(Game.creeps[creepName]);
+            }
+
+            //actionCounter.save(1500);
+        } catch (err) {
+            errorStackVisualize(err.stack);
+        }
+    });
+}; //);
